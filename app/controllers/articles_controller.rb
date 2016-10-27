f88ac823fcbcc83e0c7a4833749feb9123a9daac
Sandi Cook <sandseas1@icloud.com>
2016-10-27 23:20:51 +0000

    Form designed - now make it work

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,13 @@
+class ArticlesController < ApplicationController
+   
+   def new
+      @article = Article.new
+       
+   end
+   
+   def create
+       render plain: params[:article].inspect
+   end
+   
+    
+end
