5baf6179a3a54b188a6f50705c87a364a5c5a31f
Sandi Cook <sandseas1@icloud.com>
2016-10-28 19:31:50 +0000

    Add edit, update, show and index listing pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,19 @@
 class ArticlesController < ApplicationController
    
+   def index
+    @articles = Article.all
+   end
+  
    def new
       @article = Article.new
+   end
+   
+   def edit
+   
+    @article = Article.find(params[:id])
        
    end
+   
    
    def create
    
@@ -19,6 +29,18 @@ class ArticlesController < ApplicationController
     end
 
        
+   end
+   
+   def update
+      @article = Article.find(params[:id]) 
+      if @article.update(article_params)
+         flash[:notice] = "Article was successfully created"
+        redirect_to article_path(@article)
+    
+      else
+        render "edit"
+      end
+
    end
    
    def show
